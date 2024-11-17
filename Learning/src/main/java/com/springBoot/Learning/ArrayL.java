224b660e9d71992751e3e1612bff006b8e5dfe57
selvaraj10 <156407358+selvaraj10@users.noreply.github.com>
2024-11-17 19:33:47 +0530

    Array Learning

--- Learning/src/main/java/com/springBoot/Learning/ArrayL.java
+++ Learning/src/main/java/com/springBoot/Learning/ArrayL.java
@@ -2,14 +2,28 @@ package com.springBoot.Learning;
 
 public class ArrayL {
 
-    public int firstLargeNumber(int[] value){
+    public int firstLargeNumber(int[] value) {
         int largeNumber = 0;
-        for(int i = 0; i<value.length; i++){
+        for (int i = 0; i < value.length; i++) {
             largeNumber = value[i];
-            if(value[i] > largeNumber){
+            if (value[i] > largeNumber) {
                 largeNumber = value[i];
             }
         }
         return largeNumber;
+    }
+
+    public int secondLargestNumber(int[] value) {
+        int largest = value[0];
+        int secondLargest = Integer.MIN_VALUE;
+        for (int i = 1; i < value.length; i++) {
+            if (value[i] > largest) {
+                secondLargest = largest;
+                largest = value[i];
+            } else if (value[i] < largest && value[i] > secondLargest) {
+                secondLargest = value[i];
+            }
+        }
+        return secondLargest;
     }
 }
--- Learning/src/main/java/com/springBoot/Learning/LearningApplication.java
+++ Learning/src/main/java/com/springBoot/Learning/LearningApplication.java
@@ -4,8 +4,9 @@ public class LearningApplication {
 
     public static void main(String[] args) {
         ArrayL arrayL = new ArrayL();
-        int[] arr = {1, 2, 3, 4, 5, 7, 18};
-        System.out.println("First Largest number in Array" + arrayL.firstLargeNumber(arr));
+        int[] arr = {1, 2, 3, 4, 5, 7, 9, 18};
+        System.out.println("First Largest number in Array = " + arrayL.firstLargeNumber(arr));
+        System.out.println("Second Largest number in Array = " + arrayL.secondLargestNumber(arr));
     }
 
 }
