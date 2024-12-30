3fa0807f3a7171861219883583ac7469a64026a5
selvaraj10 <156407358+selvaraj10@users.noreply.github.com>
2024-12-30 20:48:53 +0530

    Queue Learning

---
+++ Learning/src/main/java/com/springBoot/Learning/Queue.java
@@ -0,0 +1,79 @@
+package com.springBoot.Learning;
+
+public class Queue {
+    private Node first;
+    private Node last;
+    private int length;
+
+    public Queue(int value) {
+        Node newNode = new Node(value);
+        first = newNode;
+        last = newNode;
+        length = 1;
+    }
+
+    public void getFirst() {
+        if (first == null) {
+            System.out.println("First: null");
+        } else {
+            System.out.println("First: " + first.value);
+        }
+    }
+
+    public void getLast() {
+        if (last == null) {
+            System.out.println("Last: null");
+        } else {
+            System.out.println("Last: " + last.value);
+        }
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+    public void printQueue() {
+        Node temp = first;
+        while (temp != null) {
+            System.out.println("Printing the Queue values -: " + temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void enqueue(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            first = newNode;
+        } else {
+            last.next = newNode;
+        }
+        last = newNode;
+        length++;
+    }
+
+    public Node dequeue() {
+        if (length == 0) {
+            return null;
+        }
+        Node temp = first;
+        if (length == 1) {
+            first = null;
+            last = null;
+        }
+        first = temp.next;
+        temp.next = null;
+        length--;
+        return temp;
+    }
+
+
+    public class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+
+        }
+    }
+}
